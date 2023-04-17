eca6fff071d14393f5c6d845578f9e5ad29749a2
Abhishek Singh Tomar <119028758+tomarabhishek1911@users.noreply.github.com>
2023-04-17 11:40:58 +0530

    empty

---
+++ friendsbook/profiles/forms.py
@@ -0,0 +1,9 @@
+# from django.contrib.auth import forms
+from .models import Profile
+from django import forms
+
+
+class ProfileModelForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['first_name', 'last_name', 'bio', 'avatar']
--- friendsbook/profiles/views.py
+++ friendsbook/profiles/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .models import Profile
+from .forms import ProfileModelForm
 
 # Create your views here.
+
+
 def my_profile_view(request):
-    profile=Profile.objects.get(user=request.user)
-    return render(request,'profiles/myprofile.html',{"profile":profile})
+    confirm=False
+    profile = Profile.objects.get(user=request.user)
+    if request.method == "POST":
+        fm = ProfileModelForm(request.POST, instance=profile)
+        if fm.is_valid():
+            fm.save()
+            confirm=True
+    else:
+        fm = ProfileModelForm(instance=profile)
+    return render(request, 'profiles/myprofile.html', {"form":fm,"confirm":confirm})
